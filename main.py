14516768a64316388990cfa7a31a49cb7ee0bb97
buzov <abuzov@list.ru>
2022-12-03 12:49:10 +0300

    Functions and pages added

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from flask import Flask, render_template
+from utils import *
 
 
 app = Flask(__name__)
 
 
+PATH = 'https://www.jsonkeeper.com/b/RQR9'
+candidates = load_candidates_from_json(PATH)
+
+
 @app.route('/')
 def index_page():
-    items = [1, 2, 3]
-    return render_template('index.html', items=items)
+    return render_template('index.html', candidates=candidates)
+
+
+@app.route('/candidate/<int:pk>')
+def single_page(pk):
+    return render_template('single.html', candidate=get_candidate(pk, candidates))
+
+
+@app.route('/search/<candidate_name>')
+def search_page(candidate_name):
+    search_results = get_candidates_by_name(candidate_name, candidates)
+    quantity = len(search_results)
+    return render_template('search.html', candidates=search_results, quantity=quantity)
+
+
+@app.route('/skill/<skill_name>')
+def skill_page(skill_name):
+    search_results = get_candidates_by_skill(skill_name, candidates)
+    quantity = len(search_results)
+    return render_template('skill.html', candidates=search_results, quantity=quantity)
 
 
 app.run()
--- utils.py
+++ utils.py
@@ -1,4 +1,27 @@
 import requests
 
 
-response = requests.get('https://www.jsonkeeper.com/b/RQR9').json()
+def load_candidates_from_json(path):
+    return requests.get('https://www.jsonkeeper.com/b/RQR9').json()
+
+
+def get_candidate(candidate_id, candidates):
+    for candidate in candidates:
+        if candidate_id == candidate['id']:
+            return candidate
+
+
+def get_candidates_by_name(candidate_name, candidates):
+    result = []
+    for candidate in candidates:
+        if candidate_name in candidate['name'].split():
+            result.append(candidate)
+    return result
+
+
+def get_candidates_by_skill(skill_name, candidates):
+    result = []
+    for candidate in candidates:
+        if skill_name.lower() in candidate['skills'].lower().split(', '):
+            result.append(candidate)
+    return result
